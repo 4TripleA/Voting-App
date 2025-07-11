8023e264e96fcc641b6bda4177b049fe2dcfe1ca
4TripleA <166540517+4TripleA@users.noreply.github.com>
2025-07-11 17:23:43 +0700

    updates

--- Appvote/urls.py
+++ Appvote/urls.py
@@ -6,11 +6,11 @@ from .views import CustomLogoutView
 
 
 urlpatterns = [
+    path("", auth_views.LoginView.as_view(next_page='index'), name="login"), 
     path("index/", views.index, name="index"),
     path("home/<str:slug>", views.home, name="home"),
     path("result/<str:slug>", views.result, name="result"),
     path("register/", views.register, name="register"),
-    path("", auth_views.LoginView.as_view(next_page='index'), name="login"), 
     # path("logout/", LogoutView.as_view(), name="logout")
     path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
